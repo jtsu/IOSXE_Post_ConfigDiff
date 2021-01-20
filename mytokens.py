a7b819405da969b61e90b817008b3e9f5cac1102
Jason S <jtsu@yahoo.com>
2021-01-19 22:15:56 -0800

    Update mytokens.py

--- mytokens.py
+++ mytokens.py
@@ -1,3 +1,4 @@
 access_token = "your webex token"
 teams_room = "your teams room id"
 http_proxy  = "http://your.proxy.server.com:port"
+slack_webhook_url = "https://hooks.slack.com/services/xxx/yyy/zzz"
